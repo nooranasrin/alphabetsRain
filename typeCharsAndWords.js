b3b51d4a2ea8a25680cdcaac6e2aaf0ad7287ee0
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-06 06:39:20 +0530

    added callback functions for timers

--- typeCharsAndWords.js
+++ typeCharsAndWords.js
@@ -1,5 +1,5 @@
 const { stdin, stdout } = process;
-const alphabets = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`.split(
+let alphabets = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`.split(
   ""
 );
 const charactersQue = [];
@@ -9,7 +9,7 @@ const collectAlphabetInfo = function() {
   let columns = stdout.columns - 5;
   alphabet.char = alphabets[Math.floor(Math.random() * alphabets.length)];
   alphabet.x = Math.floor(Math.random() * columns);
-  alphabet.y = 1;
+  alphabet.y = 2;
   return alphabet;
 };
 
@@ -17,7 +17,24 @@ const storeNextCharToPrint = function() {
   charactersQue.push(collectAlphabetInfo());
 };
 
+const displayAlphabets = function() {
+  stdout.cursorTo(0, 1);
+  stdout.clearScreenDown();
+  charactersQue.forEach(alphabet => {
+    stdout.cursorTo(alphabet.x, alphabet.y);
+    console.log(alphabet.char);
+    alphabet.y += 1;
+  });
+};
+
+const fallChars = function() {
+  displayAlphabets();
+  stdout.cursorTo(0, 35);
+};
+
 const main = function() {
+  alphabets = alphabets.map(alphabet => `${alphabet}\n`);
+  setInterval(fallChars, 500);
   setInterval(storeNextCharToPrint, 1000);
 };
 
